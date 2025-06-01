ac7d42b5c23baeb3d1a0d9ada9af84343280e4f9
tayoky <tayoky4848@gmail.com>
2025-06-01 19:56:52 +0200

    don't use sysroot path anymore

--- ports/tcc/configure.sh
+++ ports/tcc/configure.sh
@@ -3,23 +3,10 @@
 ./configure --prefix=$PREFIX --sysroot=$SYSROOT --targetos=stanix --enable-static --cc=$CC --triplet=x86_64-stanix --extra-cflags='-Wall
     -Wextra
     -std=gnu11
-    -ffreestanding
     -fno-stack-protector
     -fno-stack-check
     -fno-PIC
-    --sysroot='$SYSROOT'
-    -isystem='$SYSROOT'/usr/include
-	-m64
-	-march=x86-64
-	-mno-mmx
-	-mno-sse2
-	-mno-red-zone
-	-mcmodel=kernel
-	-Dx86_64' --extra-ldflags='--sysroot="'$SYSROOT'"
-    -nostdlib -nodefaultlibs
-    -static
-    '$SYSROOT'/usr/lib/crt0.o
-    -L '$SYSROOT'/usr/lib/ -lc' 
+    -static' 
 
 echo '#define CONFIG_TCC_STATIC 1
 #define CONFIG_TCC_SEMLOCK 0' >> config.h
--- ports/tutils/configure.sh
+++ ports/tutils/configure.sh
@@ -8,26 +8,10 @@ CFLAGS='-Wall\
     -fno-stack-protector \
     -fno-stack-check \
     -fno-PIC \
-    --sysroot=${SYSROOT}\
-    -isystem=${SYSROOT}/usr/include \
-	-m64 \
-	-march=x86-64 \
 	-mno-80387 \
 	-mno-mmx \
 	-mno-sse \
 	-mno-sse2 \
-	-mno-red-zone \
-	-mcmodel=kernel'
+	-mno-red-zone'
 
 ./configure --host="$HOST" --with-CC="$CC" --prefix="$PREFIX" --no-qsort --with-CFLAGS="$CFLAGS"
-
-#custom LDFLAGS not that this one only work on the patched makefile
-echo 'LDFLAGS += \
-    -nostdlib \
-    -static' >> config.mk
-
-#the original configure script don't use LD
-echo LD="$LD" >> config.mk
-
-#the configure script don't support SYSROOT
-echo "SYSROOT=$SYSROOT" >> config.mk
