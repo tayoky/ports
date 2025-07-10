d2f02c950bfaa3eae43f244de7973865979fc17f
tayoky <tayoky4848@gmail.com>
2025-07-10 17:36:24 +0200

    tutils : simplify port

--- ports/tutils/tutils.sh
+++ ports/tutils/tutils.sh
@@ -2,19 +2,8 @@ GIT=https://github.com/tayoky/tutils
 COMMIT=2e40709d590a138738dd355cdfd050317529fbab
 
 configure() {
-	#we need to specify custom CFLAGS
-	CFLAGS='-Wall\
-		-Wextra \
-		-fno-stack-protector \
-		-fno-stack-check \
-		-fno-PIC \
-		-mno-80387 \
-		-mno-mmx \
-		-mno-sse \
-		-mno-sse2 \
-		-mno-red-zone'
 
-	./configure --host="$HOST" --with-CC="$CC" --prefix="$PREFIX" --no-qsort --with-CFLAGS="$CFLAGS"
+	./configure --host="$HOST" --with-CC="$CC" --prefix="$PREFIX"
 }
 
 build() {
