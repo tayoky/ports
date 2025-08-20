e738b121de4e2fc967fc51cf2fd2e5382fd7be47
tayoky <tayoky4848@gmail.com>
2025-08-20 18:02:22 +0200

    tash : update to last tash version

--- ports/tash/tash.sh
+++ ports/tash/tash.sh
@@ -1,5 +1,5 @@
 GIT=https://github.com/tayoky/tash
-COMMIT=3fbcd3dd11895eb517deed84c6ee8ce8787cbe94
+COMMIT=601f1655b6325afbd4de11c125fcb54cb1e362a2
 
 configure() {
 	./configure --host="$HOST" --cc=$CC --prefix=$PREFIX
