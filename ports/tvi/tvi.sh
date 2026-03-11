0d49e73e3719a4aad6aedbf28b8ebffda6198e00
tayoky <tayoky4848@gmail.com>
2026-03-11 19:41:37 +0100

    tvi : update tvi

--- ports/tvi/tvi.sh
+++ ports/tvi/tvi.sh
@@ -1,5 +1,5 @@
 GIT=https://github.com/tayoky/tvi
-COMMIT=cb2b4d9f564c011e390f5c0dd706d831e86cfa7e
+COMMIT=20929ba6923388f689ef3e554dcc27aefd767f03
 
 configure() {
 	# disable dynamic linking linking
