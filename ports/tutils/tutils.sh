716a72ee017389e01d5db0166f6e6a3e903f5a17
tayoky <tayoky4848@gmail.com>
2026-03-16 18:13:00 +0100

    tutils : remove the static linking hack

--- ports/tutils/tutils.sh
+++ ports/tutils/tutils.sh
@@ -2,8 +2,7 @@ GIT=https://github.com/tayoky/tutils
 COMMIT=528eae8c46b0bbcd87679b2d6f822375fcfc5357
 
 configure() {
-	# disable dynamic linking linking
-	CFLAGS="$CFLAGS -static" ./configure --host="$HOST" --with-CC="$CC" --prefix="$PREFIX"
+	./configure --host="$HOST" --with-CC="$CC" --prefix="$PREFIX"
 }
 
 build() {
