3615506bf5abfc7969343311fe216bf40742d052
tayoky <tayoky4848@gmail.com>
2026-05-24 18:34:30 +0200

    quake2generic : fix port

--- ports/quake2generic/quake2generic.sh
+++ ports/quake2generic/quake2generic.sh
@@ -2,7 +2,7 @@ GIT=https://github.com/ozkl/quake2generic.git
 COMMIT=74d48cdb25a393032c6706344d406dc4f6f16ff3
 
 build() {
-	make -f Makefile-stanix
+	make -f Makefile-stanix -j$NPROC
 }
 
 install() {
