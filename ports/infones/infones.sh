c671279478a02d0d36fb24aadc16af1a466da852
tayoky <tayoky4848@gmail.com>
2026-07-04 19:44:07 +0200

    infones : add infones port

---
+++ ports/infones/infones.sh
@@ -0,0 +1,16 @@
+GIT=https://github.com/jay-kumogata/InfoNES
+COMMIT=4debf50a819ea3de9ddbd267ea4bc95560f35146
+
+configure () {
+	cmake -B build -S . --toolchain="$CMAKE_CROSS" -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+	-DSDL_LIBRARY="$DESTDIR/$PREFIX/lib/libSDL-1.2.so" \
+	-DSDL_INCLUDE_DIR="$DESTDIR/$PREFIX/include/SDL"
+}
+
+build() {
+	make -C build -j$NPROC
+}
+
+install() {
+	cp build/InfoNES "$DESTDIR/$PREFIX/bin"
+}
