1d48e9d47a49d7f8d363d9a9fe31d4aa091c995c
tayoky <tayoky4848@gmail.com>
2026-07-04 19:43:05 +0200

    sdl12-compat : add sdl12-compat port

---
+++ ports/sdl12-compat/sdl12-compat.sh
@@ -0,0 +1,16 @@
+GIT=https://github.com/libsdl-org/sdl12-compat
+TAG=release-1.2.76
+WEBSITE=https://libsdl.org/
+
+configure () {
+	cmake -B build -S . --toolchain="$CMAKE_CROSS" -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+	-DSDL12TESTS=OFF -DSTANIX=ON -DUNIX=ON
+}
+
+build() {
+	make -C build -j$NPROC
+}
+
+install() {
+	make -C build install DESTDIR="$DESTDIR"
+}
