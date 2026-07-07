ce2ed158c5473e24a4fe7d857abdfc8bac6493a1
tayoky <tayoky4848@gmail.com>
2026-07-07 19:12:17 +0200

    libarchive : WIP libarchive port

---
+++ ports/libarchive/libarchive.sh
@@ -0,0 +1,16 @@
+VERSION="3.8.8"
+TAR="https://www.libarchive.org/downloads/libarchive-$VERSION.tar.xz"
+WEBSITE="https://www.libarchive.org/"
+DEPENDENCIES="zlib"
+
+configure() {
+	cmake -B build -S . --toolchain="$CMAKE_CROSS" -DCMAKE_INSTALL_PREFIX="$PREFIX"
+}
+
+build() {
+	make -C build -j$NPROC
+}
+
+install() {
+	make -C build install DESTDIR="$DESTDIR"
+}
