cb4e9afa798bcb4318890670fbb72556cc518acb
tayoky <tayoky4848@gmail.com>
2026-07-13 19:02:28 +0200

    sdl2-mixer : add a sdl2 mixer port

---
+++ ports/sdl2-mixer/sdl2-mixer.sh
@@ -0,0 +1,22 @@
+GIT=https://github.com/libsdl-org/SDL_mixer
+TAG=release-2.8.2
+WEBSITE=https://libsdl.org/
+DEPENDENCIES="sdl2-compat"
+
+configure () {
+	cmake -B build -S . --toolchain="$CMAKE_CROSS" -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+	-DUNIX=ON \
+	-DSDL2MIXER_DEPS_SHARED=OFF \
+	-DSDL2MIXER_MIDI_FLUIDSYNTH=OFF \
+	-DSDL2MIXER_WAVPACK=OFF \
+	-DSDL2MIXER_OPUS=OFF \
+	-DSDL2MIXER_MOD=OFF
+}
+
+build() {
+	make -C build -j$NPROC
+}
+
+install() {
+	make -C build install DESTDIR="$DESTDIR"
+}
