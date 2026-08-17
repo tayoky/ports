cfa6ded5eaa063b6ceb88f9bfe5070f60a6edb7e
tayoky <tayoky4848@gmail.com>
2026-08-17 19:24:45 +0200

    mtools : build without iconv

--- ports/mtools/mtools.sh
+++ ports/mtools/mtools.sh
@@ -3,7 +3,8 @@ TAR="$GNU_MIRROR/mtools/mtools-$VERSION.tar.gz"
 WEBSITE="https://www.gnu.org/software/mtools/"
 
 configure() {
-	./configure --host="$HOST" --prefix="$PREFIX" CFLAGS=-Wno-error
+	./configure --host="$HOST" --prefix="$PREFIX" CFLAGS=-Wno-error \
+	ac_cv_header_iconv_h=no
 }
 
 build() {
